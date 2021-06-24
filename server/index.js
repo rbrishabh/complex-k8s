476f3328d8a805497fd8784eadcaa2ad4ad4fb5a
RISHABH BUDHIRAJA <rishabh.budhiraja@gmail.com>
2021-06-25 03:42:37 +0530

    Local deployment: complete with kubernetes

--- server/index.js
+++ server/index.js
@@ -35,23 +35,25 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-// app.get('/', (req, res) => {
-//   res.send('Hi');
-// });
 
-app.get('/values/all', async (req, res) => {
+
+app.get('/api/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/api/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/values', async (req, res) => {
+app.get('/api/', (req, res) => {
+  res.send('Hi');
+});
+
+app.post('/api/values', async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
