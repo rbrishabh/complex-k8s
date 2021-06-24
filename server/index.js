456027b243810d9fcfe908c335a3765f6fd31afe
RISHABH BUDHIRAJA <rishabh.budhiraja@gmail.com>
2021-06-25 03:29:28 +0530

    Adds ingress service

--- server/index.js
+++ server/index.js
@@ -35,9 +35,9 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/', (req, res) => {
-  res.send('Hi');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hi');
+// });
 
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
